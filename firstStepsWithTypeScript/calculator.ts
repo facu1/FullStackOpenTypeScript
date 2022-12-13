4f8ef049b8ebaffcba87673194657ea205303d8c
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-12-13 01:27:17 -0300

    add post endpoint calculate and add eslint

--- firstStepsWithTypeScript/calculator.ts
+++ firstStepsWithTypeScript/calculator.ts
@@ -1,8 +1,8 @@
-type Operation = "multiply" | "add" | "divide";
+export type Operation = "multiply" | "add" | "divide";
 
 type Result = number;
 
-const calculator = (a: number, b: number, op: Operation): Result => {
+export const calculator = (a: number, b: number, op: Operation): Result => {
   switch (op) {
     case "multiply":
       return a * b;
@@ -15,13 +15,3 @@ const calculator = (a: number, b: number, op: Operation): Result => {
       throw new Error("Operation is not multiply, add or divide!");
   }
 };
-
-try {
-  console.log(calculator(2, 4, "multiply"));
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong";
-  if (error instanceof Error) {
-    errorMessage += ` Error: ${error.message}`;
-  }
-  console.log(errorMessage);
-}
--- firstStepsWithTypeScript/index.ts
+++ firstStepsWithTypeScript/index.ts
@@ -1,9 +1,38 @@
 import express from "express";
-// const express = require("express");
+import { calculator } from "./calculator";
 const app = express();
+
+app.use(express.json());
 
 app.get("/ping", (_req, res) => {
   res.send("pong");
+});
+
+app.post("/calculate", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (!value1 || isNaN(Number(value1)) || !value2 || isNaN(Number(value2))) {
+    return res
+      .status(400)
+      .send({ error: "Provided values to calculate were not numbers!" });
+  }
+
+  if (!op) {
+    return res
+      .status(400)
+      .json({ error: "Provided operation were not valid!" });
+  }
+
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator(Number(value1), Number(value2), op);
+    return res.send(result.toString());
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) errorMessage += ` Error: ${error.message}`;
+    return res.send({ error: errorMessage });
+  }
 });
 
 const PORT = 3001;
