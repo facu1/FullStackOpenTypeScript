69d0fdb5e75ee98b1fd0485703fa5e49807a9579
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-12-13 00:32:43 -0300

    add an endpoint for the BMI calculator

--- exercises1-7/bmiCalculator.ts
+++ exercises1-7/bmiCalculator.ts
@@ -3,37 +3,29 @@ interface BmiValues {
   weight: number;
 }
 
-const parseBmiArguments = (args: string[]): BmiValues => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  else if (args.length > 4) throw new Error("Too many arguments");
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+export const parseBmiArguments = (
+  heightArg: unknown,
+  weightArg: unknown
+): BmiValues => {
+  if (!heightArg || !weightArg) {
+    throw new Error("Not height or weight argument provided");
+  }
+  if (!isNaN(Number(heightArg)) && !isNaN(Number(weightArg))) {
     return {
-      height: Number(args[2]) / 100,
-      weight: Number(args[3]),
+      height: Number(heightArg),
+      weight: Number(weightArg),
     };
   } else throw new Error("Provided values were not numbers!");
 };
 
-const calculateBmi = (height: number, weight: number) => {
-  const bmi = weight / Math.pow(height, 2);
+export const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / Math.pow(height / 100, 2);
 
   if (bmi < 18.5) {
-    return console.info("Underweight (Unhealthy)");
+    return "Underweight (Unhealthy)";
   } else if (bmi < 25) {
-    return console.info("Normal (Healthy weight)");
+    return "Normal (Healthy weight)";
   } else {
-    return console.info("Overweight (Obese)");
+    return "Overweight (Obese)";
   }
 };
-
-try {
-  const { height, weight } = parseBmiArguments(process.argv);
-  calculateBmi(height, weight);
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong.";
-  if (error instanceof Error) {
-    errorMessage += ` Error: ${error.message}`;
-  }
-  console.info(errorMessage);
-}
--- exercises1-7/index.ts
+++ exercises1-7/index.ts
@@ -1,8 +1,28 @@
 import express from "express";
+import { parseBmiArguments, calculateBmi } from "./bmiCalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height: heightQuery, weight: weightQuery } = req.query;
+
+  try {
+    const { height, weight } = parseBmiArguments(heightQuery, weightQuery);
+    const bmi = calculateBmi(height, weight);
+
+    res.json({ weight, height, bmi });
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened.";
+    if (error instanceof Error) {
+      errorMessage += ` Error: ${error.message}`;
+    }
+    res.json({
+      error: errorMessage,
+    });
+  }
 });
 
 const PORT = 3001;
