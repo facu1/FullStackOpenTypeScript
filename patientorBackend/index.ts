11ead7b726b27f086e176404fc348d7209b9e7b9
Facundo David Gonzalez <facundo.gonzalez@devactory.com>
2022-12-13 13:44:36 -0300

    initialise a new backend project. Exercise 9.8

---
+++ patientorBackend/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+app.get("/ping", (_req, res) => {
+  console.info("someone pinged here!");
+  res.send("pong");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.info(`Server running on port: ${PORT}`);
+  console.info(`http://localhost:${PORT}/ping`);
+});
